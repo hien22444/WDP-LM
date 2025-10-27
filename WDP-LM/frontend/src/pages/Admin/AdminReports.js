661ad2a6f84a68d5d0e27e623caf6d72e855a9b3
Phan Nghia <appvn147@gmail.com>
2025-10-27 20:20:25 +0700

    Restructure: Move all files from WDP-LM/ to repo root - flatten project structure

--- WDP-LM/frontend/src/pages/Admin/AdminReports.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const AdminReports = () => (
-  <div>
-    <h2>Quản lý Report</h2>
-    <div className="admin-card">Danh sách report sẽ hiển thị ở đây.</div>
-  </div>
-);
-export default AdminReports;
